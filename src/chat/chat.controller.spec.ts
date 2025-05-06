23a898b04c4884895c5698fcf37bb401bf57d0ae
Brijesh Prajapati <brijesh@incubyte.co>
2025-05-06 11:48:17 +0530

    feat: add test for service call

--- src/chat/chat.controller.spec.ts
+++ src/chat/chat.controller.spec.ts
@@ -6,18 +6,37 @@ import { ConfigService } from '@nestjs/config';
 
 describe('ChatController', () => {
   let controller: ChatController;
+  let service : ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
-      providers: [ChatService, OpenAiService, ConfigService]
+      providers: [{
+        provide : ChatService,
+        useFactory: () => {
+          return {
+            getChatResponse : jest.fn()
+          }
+        }
+      },
+     OpenAiService,
+    ConfigService]
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
+    service = module.get<ChatService>(ChatService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it("should call chat service in order to get response for given userInput", async ()=> {
+      
+      await controller.getChatCompletionMessage('userInput');
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(service.getChatResponse).toHaveBeenCalledTimes(1)
+
+  })
 
 });
